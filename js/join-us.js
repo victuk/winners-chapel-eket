8f15d2098e3746c4c910ca79ff2b619950851fc6
Victor Peter <ukokjnr@gmail.com>
2020-11-20 03:18:10 +0000

    Worked on the nav and some other pages

---
+++ js/join-us.js
@@ -0,0 +1,11 @@
+let submitButton = document.getElementById('submit-button');
+
+submitButton.addEventListener('click', function() {
+    let name = document.getElementById('fullname').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+    let testimony = document.getElementById('message').value;
+    let today = new Date();
+
+    event.preventDefault();
+}
